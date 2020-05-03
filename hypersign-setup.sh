040a117dec75d1b8cdc42b63835414af46e81f32
vishwas1 <vishwasanand.cse@gmail.com>
2020-05-03 07:58:13 +0530

    upddated with new build

--- hypersign-setup.sh
+++ hypersign-setup.sh
@@ -108,7 +108,7 @@ docker cp data-template/client-update.json  "$(docker-compose -f docker-compose.
 
 if [ ${NO_PASSWORDLESS} -eq 0 ]
 then
-  wget https://github.com/hypermine-bc/hs-authenticator/releases/download/v1.0.1/hs-authenticator.tar.gz -O dist/hs-authenticator.tar.gz
+  wget https://github.com/hypermine-bc/hs-authenticator/releases/download/v1.0.2/hs-authenticator.tar.gz -O dist/hs-authenticator.tar.gz
   docker cp dist/hs-authenticator.tar.gz "$(docker-compose -f docker-compose.yml ps -q keycloak)":/hs-authenticator.tar.gz
 
   echo -e "${BLUE_BG}Running hypersign keycloak setup script${NC}"
