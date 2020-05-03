47a4216f9b1c1c590acf1945f88babcc2aea9b7f
vishwas1 <vishwasanand.cse@gmail.com>
2020-05-03 13:28:35 +0530

    compatible with 4.8.3.final build

--- hs-script/kc-configuration.sh
+++ hs-script/kc-configuration.sh
@@ -20,6 +20,7 @@ KC_USERNAME="admin"
 KC_PASSWORD="admin"
 AUTH_FLOW_NAME="hs-auth-flow"
 KCBASE="/opt/jboss/keycloak"
+CONFIGPATH="/opt/jboss"
 
 REDIRECT_URI=$1
 CLIENT_ALIAS=$2
@@ -29,7 +30,7 @@ NO_PASSWORDLESS=$3
 echo -e "${BLUE_BG}KC-configuration setting script starts${NC}"
 
 ## Loging to keycloak
-.${KCBASE}/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user $KC_USERNAME --password $KC_PASSWORD
+${KCBASE}/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user $KC_USERNAME --password $KC_PASSWORD
 echo -e "${BLUE_BG}After log in${NC}"
 
 if [ ${NO_PASSWORDLESS} -eq 0 ]
@@ -39,7 +40,7 @@ then
     if [ -z "$IF_HS_FLOW_NOT_PRESENT" ]
     then
         echo -e "${GRAY}Creating flow: ${AUTH_FLOW_NAME} ${NC}"
-        FLOW_ID=$(.${KCBASE}/bin/kcadm.sh create authentication/flows -s alias=$AUTH_FLOW_NAME -s providerId=basic-flow -s  description=$AUTH_FLOW_NAME -s  topLevel=true  -s builtIn=false -r master 2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
+        FLOW_ID=$(${KCBASE}/bin/kcadm.sh create authentication/flows -s alias=$AUTH_FLOW_NAME -s providerId=basic-flow -s  description=$AUTH_FLOW_NAME -s  topLevel=true  -s builtIn=false -r master 2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
         echo -e "${GREEN}Creation success: flowid = $FLOW_ID ${NC}"
     else
         echo -e "${YELLOW}hs-flow is already createad. so skipping...${NC}"
@@ -48,16 +49,16 @@ then
     ##################################################
 
     ######################### HS Authenticator flow execution setting
-    IF_HSEXECUTION_NOT_PRESENT=$(.${KCBASE}/bin/kcadm.sh get authentication/flows/$AUTH_FLOW_NAME/executions --fields displayName --format csv --noquotes -r master 2>&1 | grep -w  "HyperSign QRCode")
+    IF_HSEXECUTION_NOT_PRESENT=$(${KCBASE}/bin/kcadm.sh get authentication/flows/$AUTH_FLOW_NAME/executions --fields displayName --format csv --noquotes -r master 2>&1 | grep -w  "HyperSign QRCode")
     if [ -z "$IF_HSEXECUTION_NOT_PRESENT" ]
     then
         echo -e "${GRAY}Creating execution: Hypersign QRCode in flow: ${AUTH_FLOW_NAME} ${NC}"
-        EXECUTION_ID=$(.${KCBASE}/bin/kcadm.sh create authentication/flows/$AUTH_FLOW_NAME/executions/execution -r master -s provider=hyerpsign-qrocde-authenticator -s requirement=REQUIRED  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
+        EXECUTION_ID=$(${KCBASE}/bin/kcadm.sh create authentication/flows/$AUTH_FLOW_NAME/executions/execution -r master -s provider=hyerpsign-qrocde-authenticator -s requirement=REQUIRED  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
         echo -e "${GREEN}Creation success: execid = $EXECUTION_ID ${NC}"
 
         echo -e "${GRAY}Updating execution: Hypersign QRCode${NC}"
-        sed -i "s/EXECUTION_ID/$EXECUTION_ID/g" /authenticator-flow-update.json
-        .${KCBASE}/bin/kcadm.sh update authentication/flows/$AUTH_FLOW_NAME/executions -r master  -f /authenticator-flow-update.json
+        sed -i "s/EXECUTION_ID/$EXECUTION_ID/g" $CONFIGPATH/authenticator-flow-update.json
+        ${KCBASE}/bin/kcadm.sh update authentication/flows/$AUTH_FLOW_NAME/executions -r master  -f $CONFIGPATH/authenticator-flow-update.json
     else
         echo -e "${YELLOW}Hypersign execution is already configured with $AUTH_FLOW_NAME authenticator flow. So skipping...${NC}"
     fi
@@ -65,26 +66,26 @@ then
     echo -e "${BLUE_BG}Create/Update client start ${NC}"
     ##################################################
 
-    cp /client-create.json /client-create-api.json
-    cp /client-update.json /client-update-api.json
+    cp $CONFIGPATH/client-create.json $CONFIGPATH/client-create-api.json
+    cp $CONFIGPATH/client-update.json $CONFIGPATH/client-update-api.json
 
     ########################### Registering hs-api client
     (
         API_CLIENT_ALIAS="hs-api"
-        sed -i "s/CLIENT_ALIAS/${API_CLIENT_ALIAS}/g" /client-create-api.json
-        IF_CLIENT_NOT_PRESENT=$(.${KCBASE}/bin/kcadm.sh get clients -r master --fields clientId --format csv --noquotes 2>&1 | grep -w ${API_CLIENT_ALIAS})
+        sed -i "s/CLIENT_ALIAS/${API_CLIENT_ALIAS}/g" $CONFIGPATH/client-create-api.json
+        IF_CLIENT_NOT_PRESENT=$(${KCBASE}/bin/kcadm.sh get clients -r master --fields clientId --format csv --noquotes 2>&1 | grep -w ${API_CLIENT_ALIAS})
         if [ -z "$IF_CLIENT_NOT_PRESENT" ]
         then
             echo -e "${GRAY}Creating client ${API_CLIENT_ALIAS} ${NC}"
-            CLIENT_ID=$(.${KCBASE}/bin/kcadm.sh create clients -r master -f /client-create-api.json  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
+            CLIENT_ID=$(${KCBASE}/bin/kcadm.sh create clients -r master -f /client-create-api.json  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
             echo -e "${GREEN}Client ${API_CLIENT_ALIAS} creation success: clientid = $CLIENT_ID ${NC}"
 
             echo -e "${YELLOW}Updating client ${API_CLIENT_ALIAS} ...${NC}"
-            sed -i "s/CLIENT_ID/$CLIENT_ID/g" /client-update-api.json
-            sed -i "s/FLOW_ID/$FLOW_ID/g" /client-update-api.json
-            sed -i "s/CLIENT_ALIAS/$API_CLIENT_ALIAS/g" /client-update-api.json
-            sed -i 's,VALID_REDIRECT_URI,/*,g' /client-update-api.json
-            .${KCBASE}/bin/kcadm.sh update clients/$CLIENT_ID -r master -f /client-update-api.json
+            sed -i "s/CLIENT_ID/$CLIENT_ID/g" $CONFIGPATH/client-update-api.json
+            sed -i "s/FLOW_ID/$FLOW_ID/g" $CONFIGPATH/client-update-api.json
+            sed -i "s/CLIENT_ALIAS/$API_CLIENT_ALIAS/g" $CONFIGPATH/client-update-api.json
+            sed -i 's,VALID_REDIRECT_URI,/*,g' $CONFIGPATH/client-update-api.json
+            ${KCBASE}/bin/kcadm.sh update clients/$CLIENT_ID -r master -f $CONFIGPATH/client-update-api.json
             echo -e "${YELLOW}Updating client ${API_CLIENT_ALIAS} ${NC}...${GREEN}done${NC}"
         else
             echo -e "${YELLOW}Client $API_CLIENT_ALIAS execution is already configured. So skipping...${NC}"
@@ -98,16 +99,16 @@ fi
 
 ########################### Registering your client
 (
-    sed -i "s/CLIENT_ALIAS/$CLIENT_ALIAS/g" /client-create.json
-    IF_CLIENT_NOT_PRESENT=$(.${KCBASE}/bin/kcadm.sh get clients -r master --fields clientId --format csv --noquotes 2>&1 | grep -w ${CLIENT_ALIAS})
+    sed -i "s/CLIENT_ALIAS/$CLIENT_ALIAS/g" $CONFIGPATH/client-create.json
+    IF_CLIENT_NOT_PRESENT=$(${KCBASE}/bin/kcadm.sh get clients -r master --fields clientId --format csv --noquotes 2>&1 | grep -w ${CLIENT_ALIAS})
     if [ -z "$IF_CLIENT_NOT_PRESENT" ]
     then
         echo -e "Creating client ${CLIENT_ALIAS}... ${NC}"
-        CLIENT_ID=$(.${KCBASE}/bin/kcadm.sh create clients -r master -f /client-create.json  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
+        CLIENT_ID=$(${KCBASE}/bin/kcadm.sh create clients -r master -f /client-create.json  2>&1 | sed -n "s/^.*'\(.*\)'.*$/\1/ p")
         echo -e "${GREEN}Client ${CLIENT_ALIAS} creation success: clientid = $CLIENT_ID ${NC}"
 
         echo -e "${YELLOW}Updating client ${CLIENT_ALIAS}...${NC}"
-        sed -i "s/CLIENT_ID/$CLIENT_ID/g" /client-update.json
+        sed -i "s/CLIENT_ID/$CLIENT_ID/g" $CONFIGPATH/client-update.json
 
         if [ ${NO_PASSWORDLESS} -eq 0 ] 
         then 
@@ -115,10 +116,10 @@ fi
         else
             FLOW_ID=""
         fi
-        sed -i "s/FLOW_ID/$FLOW_ID/g" /client-update.json
-        sed -i "s/CLIENT_ALIAS/$CLIENT_ALIAS/g" /client-update.json
-        sed -i "s,VALID_REDIRECT_URI,${REDIRECT_URI},g" /client-update.json
-        .${KCBASE}/bin/kcadm.sh update clients/$CLIENT_ID -r master -f /client-update.json
+        sed -i "s/FLOW_ID/$FLOW_ID/g" $CONFIGPATH/client-update.json
+        sed -i "s/CLIENT_ALIAS/$CLIENT_ALIAS/g" $CONFIGPATH/client-update.json
+        sed -i "s,VALID_REDIRECT_URI,${REDIRECT_URI},g" $CONFIGPATH/client-update.json
+        ${KCBASE}/bin/kcadm.sh update clients/$CLIENT_ID -r master -f $CONFIGPATH/client-update.json
         echo -e "${YELLOW}Updating client ${CLIENT_ALIAS} ${NC}...${GREEN}done${NC}"
     else
         echo -e "${YELLOW}Client $CLIENT_ALIAS execution is already configured. So skipping...${NC}"
--- hypersign-setup.sh
+++ hypersign-setup.sh
@@ -86,9 +86,9 @@ BLUE='\033[0;34m'
 BLUE_BG='\033[44m'
 YELLOW='\033[0;33m'  # Yellow
 NC='\033[0m' # No Color
-
-rm -rf dist
-mkdir dist
+CONFIGPATH="/opt/jboss"
+#rm -rf dist
+#mkdir dist
 
 echo -e "${BLUE_BG}Restarting docker containers...${NC}"
 docker-compose -f docker-compose.yml config
@@ -96,13 +96,13 @@ docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d --remove-orphans
 
 echo -e "${BLUE_BG}Copying required files into keycloak container...${NC}"
-docker cp hs-script/kc-webcontext.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":/kc-webcontext.sh
-docker cp hs-script/hs-plugin-install.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":/hs-plugin-install.sh
-docker cp hs-script/kc-configuration.sh  "$(docker-compose -f docker-compose.yml ps -q keycloak)":/kc-configuration.sh
+docker cp hs-script/kc-webcontext.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/kc-webcontext.sh
+docker cp hs-script/hs-plugin-install.sh "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/hs-plugin-install.sh
+docker cp hs-script/kc-configuration.sh  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/kc-configuration.sh
 
-docker cp data-template/authenticator-flow-update.json "$(docker-compose -f docker-compose.yml ps -q keycloak)":/authenticator-flow-update.json
-docker cp data-template/client-create.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":/client-create.json
-docker cp data-template/client-update.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":/client-update.json
+docker cp data-template/authenticator-flow-update.json "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/authenticator-flow-update.json
+docker cp data-template/client-create.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/client-create.json
+docker cp data-template/client-update.json  "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH/client-update.json
 
 # echo -e "${BLUE_BG}Running keycloak web context script${NC}"
 # docker-compose -f docker-compose.yml exec --user root keycloak sh /kc-webcontext.sh
@@ -110,26 +110,26 @@ docker cp data-template/client-update.json  "$(docker-compose -f docker-compose.
 if [ ${NO_PASSWORDLESS} -eq 0 ]
 then
   wget https://github.com/hypermine-bc/hs-authenticator/releases/download/v1.0.2/hs-authenticator.tar.gz -O dist/hs-authenticator.tar.gz
-  docker cp dist/hs-authenticator.tar.gz "$(docker-compose -f docker-compose.yml ps -q keycloak)":/hs-authenticator.tar.gz
+  docker cp dist/hs-authenticator.tar.gz "$(docker-compose -f docker-compose.yml ps -q keycloak)":$CONFIGPATH//hs-authenticator.tar.gz
 
   echo -e "${BLUE_BG}Running hypersign keycloak setup script${NC}"
-  docker-compose -f docker-compose.yml exec --user root keycloak sh /hs-plugin-install.sh
+  docker-compose -f docker-compose.yml exec  keycloak sh $CONFIGPATH/hs-plugin-install.sh
 
   echo -e "${BLUE_BG}Restarting keycloak server to apply changes${NC}"
   docker-compose -f docker-compose.yml restart keycloak
   docker ps
 
-  sleep 50
+  # sleep 120
 else
   echo "--no-password-less option is set. So skipping hypersign plugin installation"
   sleep 30
 fi
 
 echo -e "${BLUE_BG}Running hypersign keycloak setting script${NC}"
-docker-compose -f docker-compose.yml exec --user root keycloak sh /kc-configuration.sh ${REDIRECT_URI} ${CLIENT_ALIAS} ${NO_PASSWORDLESS}
+# docker-compose -f docker-compose.yml exec --user root keycloak sh $CONFIGPATH/kc-configuration.sh ${REDIRECT_URI} ${CLIENT_ALIAS} ${NO_PASSWORDLESS}
 
 echo -e "${BLUE_BG}Cleanup${NC}"
-docker-compose -f docker-compose.yml exec --user root keycloak rm -rf hs-authenticator* && rm -rf /*.json
+# docker-compose -f docker-compose.yml exec --user root keycloak rm -rf hs-authenticator* && rm -rf /*.json
 # rm -rf dist
 exit
 
